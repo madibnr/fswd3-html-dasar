341f12b9104db62528fbee90d742bcc55dbdeba7
mhsUPGRIS <mhs20670131@upgris.ac.id>
2023-03-22 13:26:46 +0700

    [FEAT] penambahan fitur fetching API

--- assets/js/todo_list.js
+++ assets/js/todo_list.js
@@ -1,3 +1,4 @@
+/*
 // On app load, get all tasks from localStorage
 window.onload = loadTasks;
 
@@ -109,4 +110,111 @@ function editTask(event) {
   });
   // update local storage
   localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+*/
+
+const baseUrl = "https://crudcrud.com/api/"
+
+const apiKey = "f9b826b952484353ad67fcc35b8d5a3c"
+const url = baseUrl + apiKey;
+endpointTodos = `${url}/todos`;
+
+let todos = [];
+loadTodos();
+
+function newElement() {
+  var inputValue = document.getElementById("input").value;
+  if(!inputValue){
+    alert("Todo harus di isi!");
+    return;
+  }
+  const todo = {
+    title: inputValue,
+    checked: false,
+  };
+
+  fetch(endpointTodos, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(todo),
+  })
+    .then((response) => response.json())
+    .then((todo)=>{
+      createList(todo);
+    })
+}
+
+function createList(todo) {
+  const li = document.createElement("li");
+  li.dataset.id = todo._id;
+  li.innerText = todo.title;
+  li.onclick = checkTodo;
+  var button = document.createElement("BUTTON")
+  var txt = document.createTextNode("\u00D7")
+  button.className = "close";
+  button.onclick = closeTodo;
+  button.appendChild(txt)
+  
+  if (todo.checked) {
+    li.classList.toggle("checked");
+  }
+  li.appendChild(button);
+  document.getElementById("listUL").appendChild(li);
+}
+
+function closeTodo(e) {
+  e.stopPropagation();
+  const id = this.parentElement.dataset.id;
+  let confirmed = confirm("Apakah ingin menghapus kegiatan ini?");
+  if(confirmed) {
+    if(todos != null) {
+      fetch(endpointTodos + "/" + id, {
+        method: "DELETE",
+      }).then((response) => {
+        const index = todos.findIndex((todo) => todo.id = id);
+        todos.splice(index, 1);
+        this.parentElement.remove();
+      });
+    }
+  }
+}
+
+function checkTodo() {
+  const id = this.dataset.id;
+  if (todos != null) {
+    const index = todos.findIndex((todo) => todo._id = id);
+    const todo = todos[index];
+    console.log(todo);
+    if (todo) {
+      todo.checked = !todo.checked;
+
+      fetch(endpointTodos + "/" + id, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title: todo.title,
+          checked: todo.checked,
+        }),
+      })
+        .then((updated) => updated.json())
+        .then((todo) => console.log(todo));
+        todos[index] = todo;
+    }
+  }
+  this.classList.toggle("checked");
+}
+
+function loadTodos() {
+  fetch(endpointTodos)
+      .then((response) => response.json())
+      .then((data) =>{
+            todos = data;
+            todos.forEach((todo) => {
+              createList(todo);
+            })
+      })
 }
